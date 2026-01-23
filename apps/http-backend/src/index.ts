459623bba43680ef4d64acc959c709f1be626bdc
HM-Tanjil9 <rtajil0@gmail.com>
2026-01-23 12:04:49 +0600

    Added signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,17 +3,28 @@ import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from './middleweare';
 import {CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types";
+import { prismaClient } from '@repo/db/client';
 
 const app = express()
 
 
-app.post('/signup', (req, res) => {
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success) {
+app.post('/signup', async (req, res) => {
+    const parseData = CreateUserSchema.safeParse(req.body);
+    if(!parseData.success) {
         return res.status(400).json({ error: "Invalid data" });
     }
-    const userId = 1;
-    res.json({ userId });
+    try {
+        await prismaClient.user.create({
+            data: {
+                email: parseData.data?.username,
+                password: parseData.data.password,
+                name: parseData.data.name
+            }
+        });
+        res.json({ userId: '123' });
+    } catch (e) {
+        res.status(411).json({ error: "User already exist with username" });
+    }
 });
 
 app.post('signin', (req, res) => {
---
+++ packages/db/src/index.ts
@@ -0,0 +1,4 @@
+// import { PrismaClient } from "./generated/prisma-client/index.js";
+// export const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client/extension'
+export const prismaClient = new PrismaClient()
